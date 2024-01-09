145e43682f07b719a2fd5d5b298e9fd83ba7b413
Nick Beliveau <nick@newspapersystems.com>
2024-01-09 09:50:59 -0500

    Add counter to print number of ads

--- find_ads_by_size.py
+++ find_ads_by_size.py
@@ -13,7 +13,7 @@ INPUT_FILE = "adtext.csv"
 ADMAX_DATA = "/u/abs/data/ads"
 ADMAX_SCR = "/u/abs/exe/def"
 OUTPUT_FILE = "bad_sizes.csv"
-MIN_HEIGHT = 300
+MIN_HEIGHT = 10000
 
 with open(OUTPUT_FILE, "w") as f:
 	f.write("ad_number,height\n")
@@ -27,11 +27,13 @@ with open(INPUT_FILE, "rb") as maincsv:
 
 	i = 1
 	for row in reader:
-		i += 1
-			
+
 		if ( row["Height"] != '' and float( row["Height"] ) > MIN_HEIGHT ):
+			i += 1
 			with open(OUTPUT_FILE, "a") as f:
 				f.write(row["AdNumber"] + "," + row["Height"] + "\n")
+
+print( "Number of ads : " + str(i) )
 
 ## Remove database csv file.  It could be a large file and we don't need to keep it around
 os.system('rm ' + INPUT_FILE)
