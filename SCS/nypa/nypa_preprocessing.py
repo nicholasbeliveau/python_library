e9197f475eccc0f2a96b48e6774e2ca04a2e128f
Nick Beliveau <nicholasbeliveau@gmail.com>
2024-12-26 23:38:47 -0500

    Map paper and edition code properly.  Add get date from file name for run date

--- SCS/nypa/nypa_preprocessing.py
+++ SCS/nypa/nypa_preprocessing.py
@@ -1,10 +1,13 @@
 import csv
 import os
 
-INPUT_FILE = "/home/nick/nypa_input.csv"
-OUTPUT_FILE = "/home/nick/nypa_output.csv"
-PAPER_EDITION_FILE = "paper_edition_codes.csv"
+INPUT_FILE = "ads_12.10.24.csv"
+OUTPUT_FILE = "nypa_output_20241210.csv"
+PAPER_EDITION_FILE = "site_mapping.csv"
 CATEGORY_FILE = "category_mapping.csv"
+
+def get_run_date( date ):
+  return "20" + date[10:12] + date[4:6] + date[7:9]
 
 with open( OUTPUT_FILE, "w") as f:
   f.write("confirmationId,organizationId,organizationName,organizationContactName,organizationEmail,organizationPhone,category,newspaperNamePaper,newspaperNameEdition,groupName,noticeHeightInches,numberOfColumns,firstRunDate,noticeFilePath,adType\n")
@@ -30,10 +33,18 @@ with open(INPUT_FILE, "rb") as inputcsv:
 
       paper_edition_reader = csv.DictReader((line.decode("iso8859-1").replace('\0','') for line in paperEditioncsv), delimiter=",")
       for paper_edition_row in paper_edition_reader:
-        if ( paper_edition_row["newspaperName"] == row["groupName"] ):
-          siteCode = paper_edition_row["siteCode"]
-          paper = paper_edition_row["paperID"]
-          edition = paper_edition_row["editionID"]
+        if ( paper_edition_row["site_name"].strip() == row["groupName"].strip() ):
+          siteCode = paper_edition_row["site_code"]
+
+        if ( paper_edition_row["paper_name"].strip() == row["newspaperName"].strip() ):
+          paper = paper_edition_row["paper_id"]
+          edition = paper_edition_row["edition_id"]
+
+      if ( siteCode == "" ):
+        print( row["confirmationId"] + " " + row["groupName"] + " site code not mapped correctly" )
+
+      if ( paper == "" or edition == "" ):
+        print( "." + row["newspaperName"] + "." + " paper not mapped correctly" )
 
     with open( OUTPUT_FILE, "a" ) as f:
       f.write(row["confirmationId"] + "," 
@@ -48,7 +59,7 @@ with open(INPUT_FILE, "rb") as inputcsv:
             + siteCode + ","
             + row["noticeHeightInches"] + ","
             + row["numberOfColumns"] + ","
-            + row["firstRunDate"][6:10] + row["firstRunDate"][0:2] + row["firstRunDate"][3:5] + ","
+            + get_run_date( INPUT_FILE ) + ","
             + os.path.basename(row["noticeFilePath"]) + ","
-            + "AGATE" 
+            + "Agate" 
             + "\n" )
