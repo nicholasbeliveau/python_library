e6941dffc92782840a9879e491411965265ccbd1
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-01-07 14:16:03 -0500

    Add code to remove the duplicate columns from the ads file and move it into place at the end

--- SCS/nypa/nypa_preprocessing.py
+++ SCS/nypa/nypa_preprocessing.py
@@ -1,18 +1,35 @@
+#!/usr/bin/env python3
+
 import csv
 import os
+import sys
+import pandas as pd
+import shutil
 
-INPUT_FILE = "ads_12.10.24.csv"
-OUTPUT_FILE = "nypa_output_20241210.csv"
-PAPER_EDITION_FILE = "site_mapping.csv"
+def get_run_date( file_name ):
+  return "20" + file_name[10:12] + file_name[4:6] + file_name[7:9]
+
+if len(sys.argv) > 1:
+  INPUT_FILE = sys.argv[1]
+else:
+  print( "No file passed" )
+  sys.exit()
+
+##TODO Add check for number of columns and only chop if 
+## Remove Duplicate Column Headings by number
+data = pd.read_csv(INPUT_FILE)
+data2 = data.drop( data.columns[120], axis=1 )
+data2 = data.drop( data.columns[125], axis=1 )
+data2.to_csv('temp.csv', sep=',')
+
+OUTPUT_FILE = "nypa_output_" + get_run_date( os.path.basename( INPUT_FILE ) ) + ".csv"
+PAPER_EDITION_FILE = "site_mappings.csv"
 CATEGORY_FILE = "category_mapping.csv"
-
-def get_run_date( date ):
-  return "20" + date[10:12] + date[4:6] + date[7:9]
 
 with open( OUTPUT_FILE, "w") as f:
   f.write("confirmationId,organizationId,organizationName,organizationContactName,organizationEmail,organizationPhone,category,newspaperNamePaper,newspaperNameEdition,groupName,noticeHeightInches,numberOfColumns,firstRunDate,noticeFilePath,adType\n")
 
-with open(INPUT_FILE, "rb") as inputcsv:
+with open("temp.csv", "rb") as inputcsv:
   input_reader = csv.DictReader((line.decode("iso8859-1").replace('\0','') for line in inputcsv), delimiter=",")
 
   for row in input_reader:
@@ -33,7 +50,8 @@ with open(INPUT_FILE, "rb") as inputcsv:
 
       paper_edition_reader = csv.DictReader((line.decode("iso8859-1").replace('\0','') for line in paperEditioncsv), delimiter=",")
       for paper_edition_row in paper_edition_reader:
-        if ( paper_edition_row["site_name"].strip() == row["groupName"].strip() ):
+        ##if ( paper_edition_row["site_name"].strip() == row["groupName"].strip() ):
+        if ( paper_edition_row["site_name"].strip() == row["newspaperGroupName"].strip() ):
           siteCode = paper_edition_row["site_code"]
 
         if ( paper_edition_row["paper_name"].strip() == row["newspaperName"].strip() ):
@@ -45,6 +63,8 @@ with open(INPUT_FILE, "rb") as inputcsv:
 
       if ( paper == "" or edition == "" ):
         print( "." + row["newspaperName"] + "." + " paper not mapped correctly" )
+
+      fileName = os.path.basename(row["noticeFilePath"])
 
     with open( OUTPUT_FILE, "a" ) as f:
       f.write(row["confirmationId"] + "," 
@@ -59,7 +79,11 @@ with open(INPUT_FILE, "rb") as inputcsv:
             + siteCode + ","
             + row["noticeHeightInches"] + ","
             + row["numberOfColumns"] + ","
-            + get_run_date( INPUT_FILE ) + ","
-            + os.path.basename(row["noticeFilePath"]) + ","
+            + get_run_date( os.path.basename ( INPUT_FILE ) ) + ","
+            + os.path.splitext(fileName)[0] + ","
             + "Agate" 
             + "\n" )
+      
+output_location = os.environ.get( "OutputLocation" )
+
+shutil.copy( OUTPUT_FILE, output_location )
